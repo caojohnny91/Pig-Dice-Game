b5be1708f7bd88b7aca27188dec106ec5e4c1bcd
caojohnny91 <168755794+caojohnny91@users.noreply.github.com>
2024-08-23 17:20:22 -0500

    create hidden class in CSS and set up initial game settings

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-'use strict';
+"use strict";
 
-const score1 =
+// Selecting elements
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1"); // another way to select element Id instead of querySelector, and dont need #
+const diceImg = document.querySelector(".dice");
+
+// set initial game set up
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceImg.classList.add("hidden");
